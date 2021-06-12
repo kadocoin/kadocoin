4dcdfe918d484dd754a77aef310087e497fdc716
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2021-06-12 01:44:10 -0500

    make transaction updates

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,15 @@
-import express, { Application, Request, Response, Router } from "express";
+import express, { Application, Request, Response } from "express";
 import { DEFAULT_MESSAGE } from "./config/constants";
-// import { MiddleWare } from "./middleware/middleware";
-// import { DBConfig } from "./config/DBConfig";
-// import { UserRoute } from "./routes/userRouter.router";
-// import { LoginRouter } from "./routes/loginRouter.router";
-// import { JWTMiddleWare } from "./middleware/jwt.middleware";
 class App {
   public app: Application;
 
-  private router: Router;
-
   constructor() {
     this.app = express();
-    this.router = express.Router();
     this.bootstrap();
   }
 
   bootstrap() {
-    this.app.get("/", (req: Request, res: Response, next) => {
+    this.app.get("/", (_: Request, res: Response) => {
       res.send(DEFAULT_MESSAGE);
     });
   }
