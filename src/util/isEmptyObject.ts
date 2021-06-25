a4ba1fc47f008ae36c062481867e75fa850cb22f
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2021-06-25 11:55:10 -0500

    types

--- src/util/isEmptyObject.ts
+++ src/util/isEmptyObject.ts
@@ -1,3 +1,7 @@
-export default function isEmptyObject(obj: any): boolean {
+import TransactionPool from "../wallet/transaction-pool";
+
+export default function isEmptyObject(
+  obj: Record<any, TransactionPool>
+): boolean {
   return Object.keys(obj).length === 0;
 }
