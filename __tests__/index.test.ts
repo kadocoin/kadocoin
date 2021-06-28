b702d6132a6c3b5fc0788c98e4fd184530577639
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2021-06-28 14:07:27 -0500

    jest tests

---
+++ __tests__/index.test.ts
@@ -0,0 +1,15 @@
+import Wallet from "../src/wallet/index";
+
+describe("Wallet", () => {
+  let wallet;
+
+  beforeEach(() => {
+    wallet = new Wallet();
+  });
+
+  it("has a `balance`", () => {
+    expect(wallet).toHaveProperty("balance");
+  });
+
+  // END OF WALLET
+});
