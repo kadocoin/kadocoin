bfbc734f801ac545adc0325b617455ed1a07be1d
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2021-07-07 20:15:31 -0500

    license update

--- src/util/address-filter.ts
+++ src/util/address-filter.ts
@@ -1,7 +1,9 @@
 /*
+ * # Kadocoin License
+ *
  * Copyright (c) 2021 Adamu Muhammad Dankore
  * Distributed under the MIT software license, see the accompanying
- * file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+ * file LICENSE or <http://www.opensource.org/licenses/mit-license.php>
  */
 export function filterAddress(mixedAddress: string): string {
   // param example - "msg-fee-0xC6d23c6703f33F5ad74E6E4fc17C1CE9397D4AAD"
--- src/util/crypto-hash.ts
+++ src/util/crypto-hash.ts
@@ -1,7 +1,9 @@
 /*
+ * # Kadocoin License
+ *
  * Copyright (c) 2021 Adamu Muhammad Dankore
  * Distributed under the MIT software license, see the accompanying
- * file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+ * file LICENSE or <http://www.opensource.org/licenses/mit-license.php>
  */
 import crypto from 'crypto';
 
