bd0ff0508642c14fb4987c3db291b62c7dfa79cb
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2021-07-13 14:23:49 -0500

    update test script

--- src/tinker.ts
+++ src/tinker.ts
@@ -1,24 +1,4 @@
-const data = {
-  id: '9cbae620-e1fa-11eb-b659-49d2ed53a1d1',
-  output: {
-    '0xC6d23c6703f33F5ad74E6E4fc17C1CE9397D4AAD': '936.00000000',
-    '0xf13C09968D48271991018A956C49940c41eCb1c3': '23.00000000',
-    'msg-fee-0xf13C09968D48271991018A956C49940c41eCb1c3': '35.00000000',
-    'send-fee-0xf13C09968D48271991018A956C49940c41eCb1c3': '6.00000000',
-  },
-  input: {
-    timestamp: 1625975205250,
-    amount: '1000.00000000',
-    address: '0xC6d23c6703f33F5ad74E6E4fc17C1CE9397D4AAD',
-    publicKey:
-      '0460eeaa6a2393801ca90356bab01b6d206b9a431d8475f3ebff6999eef7199ad0b0f98e2aa354b24386b072553071dfe100574667584c11b518ea1e36ba959bb4',
-    localPublicKey:
-      '049f76805dacc8a4ada763d3ce08556db328cb72aeb67de2f1fd92fe9d4bc3e3cdf43d3230b566a4d2502917532656e511097b9e67d94be0a0c31161792d0d3d20',
-    signature: {
-      r: '744e05119debdde8e0a59e21edc9688a4dd022e600f0fe62d1889408f2860aab',
-      s: 'b01603de6560aeab45820e6a5abb2974459261f39f78793f77edcc7228d7af74',
-      recoveryParam: 1,
-    },
-    message: 'akskskk kdkkd dkdkdk dkdkdkd dkdkkd',
-  },
-};
+import Wallet from './wallet';
+
+const wallet = new Wallet();
+console.log({ address: wallet.address, publicKey: wallet.publicKey });
