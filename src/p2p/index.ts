89c3304bcc2857f0941fbdec991b81c1216abd25
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2021-12-07 21:56:45 -0600

    updates

--- src/p2p/index.ts
+++ src/p2p/index.ts
@@ -208,7 +208,12 @@ class P2P {
 
     // FOR EACH PEER
     peers.forEach(peer => {
-      console.log({ peer: peer.host, local_ip });
+      console.log(
+        { peer: peer.host, local_ip },
+        sender.host != peer.host || peer.host != local_ip,
+        sender.host != peer.host && peer.host != local_ip
+      );
+
       if (sender.host != peer.host || peer.host != local_ip) {
         console.log({ forwardingTo: peer });
 
